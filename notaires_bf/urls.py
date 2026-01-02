03172a8563d2a724d923fbb616731e33522b8881
tanou-web <tanouahmadou043@gmail.com>
2026-01-02 02:31:23 +0000

    'super'

--- notaires_bf/urls.py
+++ notaires_bf/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls import re_path
+from django.urls import re_path
 from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
