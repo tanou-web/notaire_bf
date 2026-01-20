ed4f39976d6aea524188c152aca0b18601247ffa
tanou-web <tanouahmadou043@gmail.com>
2026-01-20 07:22:37 +0000

    corriger

--- notaires_bf/settings/base.py
+++ notaires_bf/settings/base.py
@@ -174,13 +174,21 @@ AUTH_USER_MODEL = 'utilisateurs.User'
 # Configuration CORS flexible
 # settings.py
 
+
 # ===== CORS =====
 if DEBUG:
-    # Développement : autoriser toutes les origines (très permissif)
-    CORS_ALLOW_ALL_ORIGINS = True
+    CORS_ALLOWED_ORIGINS = [
+
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "http://localhost:3005",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:3001",
+        "http://127.0.0.1:3002",
+        "http://127.0.0.1:3005",
+    ]
 else:
-    # Production : autoriser uniquement le front officiel et localhost pour tests
-    CORS_ALLOW_ALL_ORIGINS = False
     CORS_ALLOWED_ORIGINS = [
         "https://notaire-bf-1ns8.onrender.com",
         "https://notaire-admin-bf.onrender.com",
@@ -195,7 +203,9 @@ else:
         "http://127.0.0.1:3005",
     ]
 
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]
 CORS_ALLOW_HEADERS = [
     "accept",
