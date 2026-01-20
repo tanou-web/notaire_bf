8f7a665e8d737190a7176d736d44e51ed152730b
tanou-web <tanouahmadou043@gmail.com>
2026-01-20 00:21:51 +0000

    correige

--- apps/evenements/urls.py
+++ apps/evenements/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EvenementViewSet, InscriptionViewSet
+from .views import EvenementViewSet, InscriptionViewSet, evenement_choices
 
 router = DefaultRouter()
 router.register(r'evenements', EvenementViewSet, basename='evenement')
 router.register(r'inscriptions', InscriptionViewSet, basename='inscription')
 
 urlpatterns = [
+    path('evenements/choices/', evenement_choices, name='evenement-choices'),
     path('', include(router.urls)),
 ]
--- apps/evenements/views.py
+++ apps/evenements/views.py
@@ -18,6 +18,17 @@ class EvenementViewSet(viewsets.ModelViewSet):
             return [permissions.IsAdminUser()]
         return super().get_permissions()
 
+    # apps/evenements/views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Evenement
+
+@api_view(['GET'])
+def evenement_choices(request):
+    STATUT_CHOICES = [choice[0] for choice in Evenement.STATUT_CHOICES]
+    return Response({'statuts': STATUT_CHOICES})
+
+
 class InscriptionViewSet(viewsets.ModelViewSet):
     """API pour les inscriptions aux événements"""
     queryset = Inscription.objects.all()
